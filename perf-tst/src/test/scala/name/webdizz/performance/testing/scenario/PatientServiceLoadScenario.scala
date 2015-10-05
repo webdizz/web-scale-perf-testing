95882e7f0790db72e8b8beb7347c828bc5851c54
Izzet Mustsfaiev <webdizz@gmail.com>
2015-10-05 23:15:58 +0300

    Add notes through simulation code + change Gatling parameters due to renaming

--- perf-tst/src/test/scala/name/webdizz/performance/testing/scenario/PatientServiceLoadScenario.scala
+++ perf-tst/src/test/scala/name/webdizz/performance/testing/scenario/PatientServiceLoadScenario.scala
@@ -4,16 +4,21 @@ import io.gatling.http.Predef._
 
 import scala.concurrent.duration._
 
-class SearchForPatient extends Simulation {
+class PatientServiceLoadScenario extends Simulation {
 
   object Patient {
-    val patients = csv("patients.csv").random // default is queue, so for this test, we use random to avoid feeder starvation
+    // use data from csv file
+    val patients = csv("patients.csv").random
 
+    // load patient scenario
     val load = scenario("Patients")
+      // use feed to generate random queries
       .feed(patients)
       .exec(
         http("Load patients")
+          // use GET HTTP method
           .get("/patients/${patientId}")
+          // check response
           .check(status.is(200))
           .check(jsonPath("$.insuranceHashedId").is("${patientId}"))
       ).pause(1 second)
@@ -22,8 +27,11 @@ class SearchForPatient extends Simulation {
       .feed(patients)
       .exec(
         http("Create patient")
+          // use POST HTTP method
           .post("/patients/${patientId}")
+          // add header to specify JSON request
           .header(HttpHeaderNames.ContentType, HttpHeaderValues.ApplicationJson)
+          // encode JSON request
           .body(StringBody(
             """
               {
@@ -35,6 +43,7 @@ class SearchForPatient extends Simulation {
       ).pause(2 second)
   }
 
+  // configure common HTTP stack
   val httpConf = http
     .baseURL("http://192.168.176.128:8080")
     .acceptHeader("application/json;q=0.9,*/*;q=0.8")
@@ -42,6 +51,7 @@ class SearchForPatient extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
+  // set up load
   setUp(
     Patient.load.inject(
       rampUsers(50) over (2 minutes),
